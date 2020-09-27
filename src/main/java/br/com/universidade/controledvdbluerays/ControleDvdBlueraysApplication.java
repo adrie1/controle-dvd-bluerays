ce4e405665533b6072af86297c97b07b6cd902ad
Adriele Costa <44513370+adrie1@users.noreply.github.com>
2020-09-27 17:40:16 -0300

    Projeto Criado

---
+++ src/main/java/br/com/universidade/controledvdbluerays/ControleDvdBlueraysApplication.java
@@ -0,0 +1,13 @@
+package br.com.universidade.controledvdbluerays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDvdBlueraysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDvdBlueraysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/universidade/controledvdbluerays/ControleDvdBlueraysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.universidade.controledvdbluerays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDvdBlueraysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
